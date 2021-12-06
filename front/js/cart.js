13f539694318b2f25ead24f08d93a8b8dd4e8fdb
kanonyx <jordan.legolvan@gmail.com>
2021-12-06 10:55:03 +0100

    add displaycart

---
+++ front/js/cart.js
@@ -0,0 +1,52 @@
+
+const cart = []
+
+
+displayCart();
+
+function displayCart() {
+  const numberOfItems = localStorage.length;
+  console.log(numberOfItems);
+  for (let i = 0; i < numberOfItems; i++) {
+    const item = localStorage.getItem(localStorage.key(i));
+    const itemObject = JSON.parse(item);
+    cart.push(itemObject);
+  }
+  console.log(cart);
+  
+}
+displayCartItems();
+
+function displayCartItems() {
+  const cartItems = document.getElementById('cart__items');
+  cartItems.innerHTML = '';
+  for (let i = 0; i < cart.length; i++) {
+    const item = cart[i];
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart__item');
+    cartItem.innerHTML = `
+    <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
+    <div class="cart__item__img">
+      <img ${item.img}
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${item.name}</h2>
+        <p>${item.color}</p>
+        <p>${item.price}€</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Quantité : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${item.qty}>
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>
+    `;
+    cartItems.appendChild(cartItem);
+  }
+}
--- front/js/product.js
+++ front/js/product.js
@@ -4,8 +4,8 @@ const productId = urlParams.get('id');
 console.log(productId);
 
 fetch(`http://localhost:3000/api/products/${productId}`)
-.then(res => res.json())
-.then(result => handleData(result))
+    .then(res => res.json())
+    .then(result => handleData(result))
 
 
 
@@ -14,27 +14,30 @@ function handleData(canape) {
     console.log(canape);
     let prodImgUrl = document.querySelector('.item__img');
     let prodName = document.querySelector('#title');
-    let prodPrice = document.querySelector('#price');   
+    let prodPrice = document.querySelector('#price');
     let prodDesc = document.querySelector('#description');
     let prodColor = document.querySelector('#colors');
 
 
     prodImgUrl.innerHTML = `<img src="${canape.imageUrl}" alt="${canape.altTxt}">`
-    prodName.innerHTML = `${canape.name}` 
+    prodName.innerHTML = `${canape.name}`
     prodPrice.innerHTML = `${canape.price}`
     prodDesc.innerHTML = `${canape.description}`
 
     canape.colors.forEach(color => {
-        prodColor.innerHTML += `<option value="${color}">${color}</option>`})
+        prodColor.innerHTML += `<option value="${color}">${color}</option>`
+    })
 
 
-        
+
 }
 
 const button = document.querySelector('#addToCart');
+
 button.addEventListener('click', () => {
     let color = document.querySelector('#colors').value;
     let qty = document.querySelector('#quantity').value;
+    if (color == null || color == "" || qty == null || qty == "") { alert("Choisissez une couleur et une quantité"); return; }
     let price = document.querySelector('#price').innerHTML;
     let name = document.querySelector('#title').innerHTML;
     let img = document.querySelector('.item__img').innerHTML;
@@ -48,7 +51,6 @@ button.addEventListener('click', () => {
         qty: qty,
     }
     console.log(cart);
-    localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem(id, JSON.stringify(cart));
     window.location.href = 'cart.html';
 })
-    
