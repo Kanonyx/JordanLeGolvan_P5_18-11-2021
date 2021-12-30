7448b6260ddcc1dee56226300d6a17766ba885da
kanonyx <jordan.legolvan@gmail.com>
2021-12-30 14:09:56 +0100

    code optimisation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -3,6 +3,10 @@ const orderId = getOrderId();
 displayOrderId(orderId);
 clearSessionStorage();
 
+
+
+
+//recupere l'id de la commande dans l'url
 function getOrderId() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -10,11 +14,18 @@ function getOrderId() {
     return orderId;
 }
 
+
+
+//affiche l'id de la commande dans l'html
 function displayOrderId(orderID) {
     const orderIdElement = document.getElementById('orderId');
     orderIdElement.textContent = orderId;
 }
 
+
+
+
+//supprime toutes les données de la session storage
 function clearSessionStorage() {
  const cache = window.localStorage;
  cache.clear();
